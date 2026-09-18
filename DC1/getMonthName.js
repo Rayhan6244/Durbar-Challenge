b871c47dc3fa6d52da10cce407645841c5ca2ebe
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2026-09-19 01:30:38 +0600

    Create getMonthName.js

---
+++ DC1/getMonthName.js
@@ -0,0 +1,8 @@
+function getMonthName(monthNumber) {
+    const months = [
+        "January", "February", "March", "April", 
+        "May", "June", "July", "August", 
+        "September", "October", "November", "December"
+    ];
+    return months[monthNumber - 1];
+}
